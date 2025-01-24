f11c169e5acd3d173614c973f0161d86842369b6
nalaura <analauraej7@gmail.com>
2025-01-23 23:19:18 -0300

    Delete inutil migration

--- prisma/migrations/20250124015409_default_categories_and_category_relation_task/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Task_categoryId_fkey` ON `task`;
-
--- AddForeignKey
-ALTER TABLE `Task` ADD CONSTRAINT `Task_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
